06e987d8f4fa3edbea221aabdf8b94e9bcfa02a2
ademch <ntuu.kpi@ukr.net>
2024-11-29 09:50:09 +0200

    signal CHLD and INT handled

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
 #include <stdio.h>
+#include <sys/wait.h>
 #include "Aztec.h"
 
 int CreateHTTPserver();
 
+void sigchldHandler(int s)
+{
+	printf("Caught signal SIGCHLD\n");
+
+	pid_t pid;
+	int status;
+
+	while ((pid = waitpid(-1,&status,WNOHANG)) > 0)
+	{
+		if (WIFEXITED(status)) printf("\nChild process terminated");
+	}
+}
+
+void sigintHandler(int s)
+{
+	printf("Caught signal %d. Starting graceful exit procedure\n",s);
+
+	pid_t pid;
+	int status;
+	while ((pid = waitpid(-1,&status,0)) > 0)
+	{
+		if (WIFEXITED(status)) printf("\nChild process terminated");
+	}
+	
+	if (pid == -1) printf("\nAll child processes terminated");
+
+	exit(EXIT_SUCCESS);
+}
+
 
 int main(int argc, char* argv[])
 {
+	signal(SIGCHLD, sigchldHandler);
+	signal(SIGINT, sigintHandler);
+
 	Aztec aztec;
 
 	aztec.AddString("The first string");
